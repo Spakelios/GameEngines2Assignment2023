c69f3848f4ff7c320c1a6bd3d51b7c206c80610c
Glopex <62385400+Glopex@users.noreply.github.com>
2023-04-13 00:23:05 +0100

    map falloff generation

--- NatureSim/Assets/gabs folder/maptest.cs
+++ NatureSim/Assets/gabs folder/maptest.cs
@@ -11,12 +11,17 @@ public class maptest : MonoBehaviour
     public int[] newTriangles;
     public int xSize = 20;
     public int zSize = 20;
+    [SerializeField] float meshrangex;
+    [SerializeField] float meshrangey;
 
+    public float fallofValue;
+    public float heightCheck;
     void Start()
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         CreateShape();
+        CreateHeight();
         UpdateMesh();
     }
 
@@ -33,7 +38,34 @@ public class maptest : MonoBehaviour
         mesh.triangles = newTriangles;
         mesh.RecalculateNormals();
     }
+    void CreateHeight()
+    {
+        float test;
+        float[,] noiseMap = new float[xSize, xSize];
+        for (int z = 0, i = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
 
+                float randx = Random.Range(1f, meshrangex);
+                float randy = Random.Range(1f, meshrangey);
+
+                float y = Mathf.PerlinNoise(randx, randy);
+
+                float xv = x / (float)xSize * 2 - 1;
+                float zv = z / (float)xSize * 2 - 1;
+                float v = Mathf.Max(Mathf.Abs(xv), Mathf.Abs(zv));
+                test = Mathf.Pow(v, fallofValue) / (Mathf.Pow(v, fallofValue) + Mathf.Pow(2.2f - 2.2f * v, fallofValue));
+                print(test);
+                y -= test;
+                if (y < heightCheck)
+                    y = -0.1f;
+                else y = 0.1f;
+                newVertices[i].y = y;
+                i++;
+            }
+        }
+    }
     void CreateShape()
     {
         newVertices = new Vector3[(xSize + 1) * (zSize + 1)];
@@ -42,8 +74,8 @@ public class maptest : MonoBehaviour
         {
             for (int x = 0; x <= xSize; x++)
             {
-                float y = Mathf.PerlinNoise(Random.Range(0f,3f) * 2f, Random.Range(0f,3f));
-                newVertices[i] = new Vector3(x, y, z);
+                
+                newVertices[i] = new Vector3(x, 0, z);
                 i++;
             }
         }
