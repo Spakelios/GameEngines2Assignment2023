1af7ab2ca646ff1bf53d8d81a238a19a00b230d4
Spakelios <73300254+Spakelios@users.noreply.github.com>
2023-05-02 23:27:31 +0100

    snake

--- NatureSim/Assets/Kellies Stuff/Code/SnakeState.cs
+++ NatureSim/Assets/Kellies Stuff/Code/SnakeState.cs
@@ -6,13 +6,17 @@ public class SnakeState : SlimeBaseState
 {
     public override void EnterState(FoodStates food)
     {
-      Debug.Log("Solid Snake");
-      food.tag = "Snake";
+        Debug.Log("Solid Snake");
+        food.tag = "Snake";
+        food.GetComponent<Animator>().Play("Snaaaaaaaaaakkkeeee!");
     }
 
     public override void UpdateState(FoodStates food)
     {
-       
+        if (food.GetComponent<SlimeStats>().foodThree >= 3)
+        {
+            food.GetComponent<Animator>().Play("SnakeLegs");
+        }
     }
 
     public override void OnCollisionEnter(FoodStates food)
