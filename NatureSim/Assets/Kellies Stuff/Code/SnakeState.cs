d7e7058112457b196d15b885d09811445b35f855
Spakelios <73300254+Spakelios@users.noreply.github.com>
2023-05-02 23:52:25 +0100

    Revert "snake"
    This reverts commit 1af7ab2ca646ff1bf53d8d81a238a19a00b230d4.

--- NatureSim/Assets/Kellies Stuff/Code/SnakeState.cs
+++ NatureSim/Assets/Kellies Stuff/Code/SnakeState.cs
@@ -6,17 +6,13 @@ public class SnakeState : SlimeBaseState
 {
     public override void EnterState(FoodStates food)
     {
-        Debug.Log("Solid Snake");
-        food.tag = "Snake";
-        food.GetComponent<Animator>().Play("Snaaaaaaaaaakkkeeee!");
+      Debug.Log("Solid Snake");
+      food.tag = "Snake";
     }
 
     public override void UpdateState(FoodStates food)
     {
-        if (food.GetComponent<SlimeStats>().foodThree >= 3)
-        {
-            food.GetComponent<Animator>().Play("SnakeLegs");
-        }
+       
     }
 
     public override void OnCollisionEnter(FoodStates food)
